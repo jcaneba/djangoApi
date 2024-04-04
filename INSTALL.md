67a45b1ea46da0d5932f83a45b3ebde717d98e3f
jcaneba <jesuscanebatic@gmail.com>
2024-04-04 20:24:24 +0200

    Quickstart de la aplicación.
    Se crean los serializadores y las vistas de los
    modelos User y Group internos. Se configura el
    settings.py con la base de datos y las opciones del
    rest framework.

--- INSTALL.md
+++ INSTALL.md
@@ -1,6 +1,7 @@
+sudo apt-get install libmysqlclient-dev
 python3 -m venv ./venv
 source ./venv/bin/activate
-pip3 install django djangorestframework
+pip3 install django djangorestframework mysqlclient
 django-admin startproject api
 cd api
 python manage.py startapp main
--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoapi',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '172.19.0.2',
+        'PORT': '3306'
     }
 }
 
@@ -121,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
